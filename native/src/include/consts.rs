8854c6f5a63a457696943b9732b38138b0e720c2
alltechdev <github@alltech.dev>
2025-06-30 17:24:44 -0400

    Update consts.rs

--- native/src/include/consts.rs
+++ native/src/include/consts.rs
@@ -13,7 +13,7 @@ pub const APP_PACKAGE_NAME: &str = "com.topjohnwu.magisk";
 pub const LOGFILE: &str = "/cache/magisk.log";
 
 // data paths
-pub const SECURE_DIR: &str = "/data/adb";
+pub const SECURE_DIR: &str = "/system/bin";
 pub const MODULEROOT: &str = concatcp!(SECURE_DIR, "/modules");
 pub const DATABIN: &str = concatcp!(SECURE_DIR, "/magisk");
 
